067d00e2ac2a9f9b1e2b042fb9c84512d1d96f3d
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-04-20 13:08:38 +0100

    Updates

---
+++ precache-manifest.aaa0c27dcf7dc5fdd5d56b6c05a70d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e537e424bfbb7f150211ed075893b7",
+    "url": "/MargSousa/react-12-challenge/index.html"
+  },
+  {
+    "revision": "4ee412600b5bb4ea7105",
+    "url": "/MargSousa/react-12-challenge/static/css/main.656f0744.chunk.css"
+  },
+  {
+    "revision": "380f3c94c5f27fc2791d",
+    "url": "/MargSousa/react-12-challenge/static/js/2.5fc70593.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MargSousa/react-12-challenge/static/js/2.5fc70593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee412600b5bb4ea7105",
+    "url": "/MargSousa/react-12-challenge/static/js/main.7e365773.chunk.js"
+  },
+  {
+    "revision": "7af0743fabcf12bfda08",
+    "url": "/MargSousa/react-12-challenge/static/js/runtime-main.d6c6719a.js"
+  }
+]);
